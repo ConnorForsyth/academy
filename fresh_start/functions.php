b775d5bf935cef1caf60dec4ddfaff9e48e10938
Connor Forsyth <connor.forsyth@aberdeenshire.gov.uk>
2018-06-08 09:41:48 +0100

    The Basics

--- fresh_start/functions.php
+++ fresh_start/functions.php
@@ -120,7 +120,24 @@ add_action( 'widgets_init', 'fresh_start_widgets_init' );
  * Enqueue scripts and styles.
  */
 function fresh_start_scripts() {
+
 	wp_enqueue_style( 'fresh_start-style', get_stylesheet_uri() );
+
+	/*Shown by tutorial - This loads bootstrap when the page is loaded */
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() .'/css/bootstrap.min.css',array(),'3.3.4' );
+
+	if( !is_admin()){
+		wp_deregister_script( 'jquery' );
+		wp_register_script('jquery', get_template_directory_uri().'/js/jquery.min.js', false,'1.10.2',true);
+		wp_enqueue_script('jquery');
+	}
+	wp_enqueue_script( 'bootstrap-min-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', true );
+	wp_enqueue_script( 'firstheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'firstheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/js/custom-js.js', array(), '1.0', true );
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 
 	wp_enqueue_script( 'fresh_start-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
@@ -158,4 +175,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
